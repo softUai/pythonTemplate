4ef10ec48c939c9d691eb8e547e62593acc6c4eb
unknown <garcia.pedro.clair@gmail.com>
2024-06-01 14:21:30 -0300

    project with create schem and architeture

--- database/connect.py
+++ database/connect.py
@@ -1,6 +1,9 @@
 import mysql.connector
 import pyenv.private as pvt
 
+def dbConnect():
+  return mysql.connector.connect( host=pvt.HOST, user=pvt.USER, password=pvt.PASSWORD )
+
 def dbConnect(dbName):
   return mysql.connector.connect( host=pvt.HOST, user=pvt.USER, password=pvt.PASSWORD, database=dbName )
 
